a7d5d4d3bb88441271f6c8696599afa1ac77223f
pyollin <48953989+pyollin@users.noreply.github.com>
2020-07-27 21:51:31 -0700

    Add files via upload

--- testing_false_cal_file.py
+++ testing_false_cal_file.py
@@ -1,12 +1,14 @@
 #%%
 # Paul Yollin
-# 07/09/2020
+# started:      07/09/2020
+# last update:  07/27/2020
 
-# creating a false calibration file to then test reducation factors
+# creating a false calibration file to then test reduction factors
 
 
 import Funfile as Fun
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 params = {
@@ -17,16 +19,49 @@ params = {
     '105': {'start': 65, 'stop': 105}
 }
 
-rec= '5'
+xs = np.arange(1, 8)
+
+rec = '5'
 file_name = rec + params['f_ext']
 
-diag_cal_mat = np.diag(np.append(np.linspace(-0.5, 0.5, 306), np.ones(27)))
+#   Creating vector w/ first 306 elements randomly distributed (1,2], last 27 = 1
+diagonal = np.append(1+np.random.rand(306), np.ones(27))
 
-dat = Fun.RecClass_Order_Cal(file_name=file_name,
-                             start=params[rec]['start'],
-                             stop=params[rec]['stop'],
-                             internal=2,
-                             external=2,
-                             diag_cal_mat=diag_cal_mat)
+#   Creating a dictionary holding calibration error matrix and an identity matrix
+Mats = {
+    'Identity': np.diag(np.ones(333)),
+    'Cal_error': np.diag(diagonal)
+}
 
-drop = Fun.find_drop_order(dat.data)
+#   creating figure and axes to plot vector names 'diagonal'
+fig, ax = plt.subplots(1, 1, figsize=(13,7))
+
+#   plotting vector, adding gridlines, setting titles and axes labels
+ax.plot(diagonal)
+ax.grid()
+ax.set(title='Diagonal values of generated calibration errors matrix',
+       xlabel='Diagonal entry',
+       ylabel='Value')
+
+#%%
+
+#   Creating dictionary of empty dictionaries to hold calculated reduction factors
+drop = {
+    'Identity' : {},
+    'Cal_error' : {}
+}
+
+#   for loop going through and calculating reduction factors
+for e in ['Identity', 'Cal_error']:
+    for i in xs:
+        dat = Fun.RecClass_Order_Cal(file_name=file_name,
+                                     start=params[rec]['start'],
+                                     stop=params[rec]['stop'],
+                                     internal=2,
+                                     external=i,
+                                     diag_cal_mat=Mats[e])
+        drop[str(e)][str(i)] = Fun.find_drop_order(dat.data)
+del i, e
+
+#   using plot_drop_order function found in FunFile to plot reduction factors
+Fun.plot_drop_order(drop)
