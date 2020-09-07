22de192c2dad2ef84feed08df2804cc029b39934
pyollin <48953989+pyollin@users.noreply.github.com>
2020-09-07 08:02:04 -0700

    Add files via upload

--- Funfile.py
+++ Funfile.py
@@ -22,6 +22,7 @@ from numpy import linalg as LA
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import pandas as pd
 
 
 class RecClass:
@@ -44,10 +45,10 @@ class RecClass:
         # marking bad channels
         self.raw.info['bads'] = ['MEG1433', 'MEG1842', 'MEG1743']
         # Applying SSS without using the calibration file
-        self.rawSSS = mne.preprocessing.maxwell_filter(self.raw,
-                                                       coord_frame='meg',
-                                                       verbose='warning',
-                                                       calibration=self.calibration_file_old)
+        # self.rawSSS = mne.preprocessing.maxwell_filter(self.raw,
+        #                                                coord_frame='meg',
+        #                                                verbose='warning',
+        #                                                calibration=self.calibration_file_old)
         # Applying SSS using the calibration file
         self.rawSSSc = mne.preprocessing.maxwell_filter(self.raw,
                                                         coord_frame='meg',
@@ -61,7 +62,7 @@ class RecClass:
                      'fc_mdat': self.rawSSSc.get_data(start=start*1000, stop=stop*1000, picks='mag'),
                      'fc_gdat': self.rawSSSc.get_data(start=start*1000, stop=stop*1000, picks='grad'),
                      'r_whole': self.raw.get_data(start=start*1000, stop=stop*1000),
-                     'f_whole': self.rawSSS.get_data(start=start*1000, stop=stop*1000),
+                     # 'f_whole': self.rawSSS.get_data(start=start*1000, stop=stop*1000),
                      'fc_whole': self.rawSSSc.get_data(start=start*1000, stop=stop*1000)}
         # setting the bad gradiometer channels from the raw gradiometere array to zero
         self.data['r_gdat'][104,:] = 0
@@ -349,45 +350,30 @@ class RecClass_Order_Cal:
                                                        int_order=int(internal),
                                                        ext_order=int(external),
                                                        verbose='warning',
-                                                       # calibration=self.calibration_file_new
+                                                       calibration=self.calibration_file_new
                                                        )
         # creates data arrays holding the recordings for easier analysis
         self.data = {'r_mdat': self.new_raw.get_data(start=start*1000, stop=stop*1000, picks='mag'),
                      'r_gdat': self.new_raw.get_data(start=start*1000, stop=stop*1000, picks='grad'),
-                     'f_mdat': self.rawSSS.get_data(start=start*1000, stop=stop*1000, picks='mag'),
-                     'f_gdat': self.rawSSS.get_data(start=start*1000, stop=stop*1000, picks='grad')}
+                     'fc_mdat': self.rawSSS.get_data(start=start*1000, stop=stop*1000, picks='mag'),
+                     'fc_gdat': self.rawSSS.get_data(start=start*1000, stop=stop*1000, picks='grad')}
         # setting the bad gradiometer channels from the raw gradiometer array to zero
         self.data['r_gdat'][104, :] = 0
         self.data['r_gdat'][130, :] = 0
         self.data['r_gdat'][139, :] = 0
 
 
-def cal_error(file_name, diag_cal_mat):
-
-    data_path = '/Users/cyoll/OneDrive/Documents/I-Labs/Research/Eddy_Current/eddy_current3/'
-
-    raw = (mne.io.read_raw_fif(data_path + str(file_name),
-                               allow_maxshield=True,
-                               verbose='warning')).fix_mag_coil_types()
-
-    data = raw.get_data()
-    new_dat = np.matmul(diag_cal_mat, data)
-    new_raw = mne.io.RawArray(new_dat, raw.info)
-
-    return new_raw
-
-
 def find_drop_order(data):
 
     norms = {}
 
-    for i in ['r_mdat', 'r_gdat', 'f_mdat', 'f_gdat']:
+    for i in ['r_mdat', 'r_gdat', 'fc_mdat', 'fc_gdat']:
         norms[i] = LA.norm(data[i], axis=0)
     del i
 
     rms = {}
 
-    for i in ['r_mdat', 'r_gdat', 'f_mdat', 'f_gdat']:
+    for i in ['r_mdat', 'r_gdat', 'fc_mdat', 'fc_gdat']:
         rms[i] = np.sqrt(np.dot(norms[i], norms[i]))
     del i
 
@@ -397,23 +383,25 @@ def find_drop_order(data):
 
         name = i + 'dat'
 
-        drop[name] = rms['r_' + name] / rms['f_' + name]
+        drop[name] = rms['r_' + name] / rms['fc_' + name]
 
     return drop
 
 
 def plot_drop_order(drops):
 
-    xs = np.arange(1, 8)
+    num_expan = len(drops['Identity'])
+
+    xs = np.arange(1, (num_expan + 1))
 
     mdrop = {
-        'Identity': np.zeros(7),
-        'Cal_error': np.zeros(7)
+        'Identity': np.zeros(num_expan),
+        'Cal_error': np.zeros(num_expan)
     }
 
     gdrop = {
-        'Identity': np.zeros(7),
-        'Cal_error': np.zeros(7)
+        'Identity': np.zeros(num_expan),
+        'Cal_error': np.zeros(num_expan)
     }
 
     fig, ax = plt.subplots(1, 2, figsize=(13, 7))
@@ -438,3 +426,140 @@ def plot_drop_order(drops):
     ax[1].set(title='Gradiometer Drops over expansion order',
               xlabel='Expansion order (external)',
               ylabel='Reduction Factor')
+
+
+class RecClass_August_Report:
+
+    # This class is the same a RecClass in the beginning of FunFile but it will specify the orders of expansion for the
+
+    # Path to the .fif file of the recording, and path to the SSS fine calibration file (machine specific!!)
+    data_path = '/Users/cyoll/OneDrive/Documents/I-Labs/Research/Eddy_Current/eddy_current3/'
+    calibration_file_new = '/Users/cyoll/OneDrive/Documents/I-Labs/Research/Eddy_Current/Analysis/sss_cal_new.dat'
+
+    # initializing the object
+    def __init__(self, file_name, start, stop, internal, external):
+
+        # reading in raw file and also fixing coil types
+        self.raw = (mne.io.read_raw_fif(
+            self.data_path + str(file_name),
+            allow_maxshield=True,
+            verbose='warning'
+        )).fix_mag_coil_types()
+
+        # marking bad channels
+        self.raw.info['bads'] = ['MEG1433', 'MEG1842', 'MEG1743']
+        # Applying SSS without using the calibration file
+        self.raw_SSS = mne.preprocessing.maxwell_filter(
+            self.raw,
+            coord_frame='meg',
+            int_order=int(internal),
+            ext_order=int(external),
+            verbose='warning',
+        )
+        # Applying SSS with the with the fine calibration file
+        self.raw_f_SSS = mne.preprocessing.maxwell_filter(
+            self.raw,
+            coord_frame='meg',
+            int_order=int(internal),
+            ext_order=int(external),
+            verbose='warning',
+            calibration=self.calibration_file_new
+        )
+        # creates data arrays holding the recordings for easier analysis
+        self.data = {'r_mdat': self.raw.get_data(start=start*1000, stop=stop*1000, picks='mag'),
+                     'r_gdat': self.raw.get_data(start=start*1000, stop=stop*1000, picks='grad'),
+                     'f_mdat': self.raw_SSS.get_data(start=start * 1000, stop=stop * 1000, picks='mag'),
+                     'f_gdat': self.raw_SSS.get_data(start=start * 1000, stop=stop * 1000, picks='grad'),
+                     'fc_mdat': self.raw_f_SSS.get_data(start=start*1000, stop=stop*1000, picks='mag'),
+                     'fc_gdat': self.raw_f_SSS.get_data(start=start*1000, stop=stop*1000, picks='grad')}
+        # setting the bad gradiometer channels from the raw gradiometer array to zero
+        self.data['r_gdat'][104, :] = 0
+        self.data['r_gdat'][130, :] = 0
+        self.data['r_gdat'][139, :] = 0
+
+
+def plot_drop_August_Report(drops, title_name):
+    #   finding the number of external expansions for x-axis
+    num_expan = len(drops)
+
+    #   defning the values for the x-axis
+    x_vals = np.arange(1, (num_expan + 1))
+
+    #   empty dictionary to hold magnetometer reduction factors
+    mdrop = {
+        'w/o_fine_cal': np.zeros(num_expan),
+        'w/_fine_cal': np.zeros(num_expan)
+    }
+
+    #   empty dictionary to hold gradiomter reduction factors
+    gdrop = {
+        'w/o_fine_cal': np.zeros(num_expan),
+        'w/_fine_cal': np.zeros(num_expan)
+    }
+
+    #   making empty figure and two axes to plot onto
+    fig, ax = plt.subplots(1, 2, figsize=(13, 7))
+
+    #   for loop extracting the reduction factors so they can be plotted
+    for e in x_vals:
+        mdrop['w/o_fine_cal'][e - 1] = drops[str(e)]['mdat']
+        gdrop['w/o_fine_cal'][e - 1] = drops[str(e)]['gdat']
+
+    #   plotting the reduction factors
+    ax[0].plot(x_vals, mdrop['w/o_fine_cal'], label='w/o_fine_cal')
+    ax[1].plot(x_vals, gdrop['w/o_fine_cal'], label='w/o_fine_cal')
+    del e
+
+    for e in x_vals:
+        mdrop['w/_fine_cal'][e - 1] = drops[str(e)]['f_mdat']
+        gdrop['w/_fine_cal'][e - 1] = drops[str(e)]['f_gdat']
+
+    #   plotting the reduction factors
+    ax[0].plot(x_vals, mdrop['w/_fine_cal'], label='w/_fine_cal')
+    ax[1].plot(x_vals, gdrop['w/_fine_cal'], label='w/_fine_cal')
+    del e  # deleting the variable used in the for loop
+
+    ax[0].legend(loc='upper right')  # location for legend
+    ax[1].legend(loc='upper right')  # location for legend
+    ax[0].grid()  # turning grid lines on
+    ax[1].grid()  # turning grid lines on
+    ax[0].set(title= title_name +' Magnetometer Drops over expansion order',
+              xlabel='Expansion order (external)',
+              ylabel='Reduction Factor')  # setting titles and axis labels
+
+    ax[1].set(title= title_name + ' Gradiometer Drops over expansion order',
+              xlabel='Expansion order (external)',
+              ylabel='Reduction Factor')
+
+
+def find_drop_August_Report(data):
+
+    norms = {}
+
+    for i in ['r_mdat', 'r_gdat', 'f_mdat', 'f_gdat', 'fc_mdat', 'fc_gdat']:
+        norms[i] = LA.norm(data[i], axis=0)
+    del i
+
+    rms = {}
+
+    for i in ['r_mdat', 'r_gdat', 'f_mdat', 'f_gdat', 'fc_mdat', 'fc_gdat']:
+        rms[i] = np.sqrt(np.dot(norms[i], norms[i]))
+    del i
+
+    drop = {}
+
+    for i in ['m', 'g']:
+
+        name = i + 'dat'
+
+        drop[name] = rms['r_' + name] / rms['f_' + name]
+    del i
+
+    for i in ['m', 'g']:
+
+        name = i + 'dat'
+
+        drop['f_'+ name] = rms['r_' + name] / rms['fc_' + name]
+    del i
+
+    return drop
